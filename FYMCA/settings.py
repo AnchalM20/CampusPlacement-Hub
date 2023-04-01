50f9f46bd3c489dea554064f533d3ae9f38a0d70
AnchalM20 <anchallsm2000@gmail.com>
2023-04-01 21:35:18 +0530

    added setting for deploying project on Heroku

--- FYMCA/settings.py
+++ FYMCA/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--2d1#oa%uvpfltpfdut7h-y%i)1_l8pt3togro1hh0e#oq)a3a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [127.0.0.1]
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
